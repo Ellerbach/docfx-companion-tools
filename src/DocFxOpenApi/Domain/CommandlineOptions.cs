40874d204b990f35f014674440df620194570459
Fabricio Ferreira <fabricio.ferreira@soeidental.com>
2024-04-24 16:24:44 +1200

    Added support to specify a single source file

--- src/DocFxOpenApi/Domain/CommandlineOptions.cs
+++ src/DocFxOpenApi/Domain/CommandlineOptions.cs
@@ -3,29 +3,61 @@
 
 namespace DocFxOpenApi.Domain
 {
+    using System.IO;
     using CommandLine;
 
     /// <summary>
-    /// Class for command line options.
+    ///     Class for command line options.
     /// </summary>
     public class CommandlineOptions
     {
         /// <summary>
-        /// Gets or sets the folder with specifications.
+        ///     Gets or sets the folder with specifications.
         /// </summary>
-        [Option('s', "specfolder", Required = true, HelpText = "Folder containing the OpenAPI specification.")]
-        public string? SpecFolder { get; set; }
+        [Option('s', "specsource", Required = true, HelpText = "Folder or File containing the OpenAPI specification.")]
+        public string? SpecSource
+        {
+            get => SpecFolder ?? SpecFile;
+            set => SetSource(value);
+        }
 
         /// <summary>
-        /// Gets or sets the output folder.
+        ///     Gets or sets the output folder.
         /// </summary>
         [Option('o', "outputfolder", Required = false, HelpText = "Folder to write the resulting specifications in.")]
         public string? OutputFolder { get; set; }
 
         /// <summary>
-        /// Gets or sets a value indicating whether verbose information is shown in the output.
+        ///     Gets or sets a value indicating whether verbose information is shown in the output.
         /// </summary>
         [Option('v', "verbose", Required = false, HelpText = "Show verbose messages.")]
         public bool Verbose { get; set; }
+
+        /// <summary>
+        ///     Gets the folder with specifications, if the source is a folder.
+        /// </summary>
+        public string? SpecFolder { get; private set; }
+
+        /// <summary>
+        ///     Gets the file with specifications, if the source is a file.
+        /// </summary>
+        public string? SpecFile { get; private set; }
+
+        private void SetSource(string? value)
+        {
+            if (value == null)
+            {
+                return;
+            }
+
+            if (Directory.Exists(value))
+            {
+                SpecFolder = value;
+            }
+            else if (File.Exists(value))
+            {
+                SpecFile = value;
+            }
+        }
     }
 }
